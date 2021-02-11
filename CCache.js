6de7d900daf69db19122bb377c893d59e8dcc094
Kushal Niroula <niroulaKushal31@gmail.com>
2021-02-11 20:06:15 +0545

    Add CCache.js

---
+++ CCache.js
@@ -0,0 +1,28 @@
+const CCache = {
+    Cache: new Map(),
+    Set(key, value) {
+        this.Cache.set(key, value);
+        return this;
+    },
+    Get(key,defaultValue) {
+        const data = this.Cache.get(key);
+        if(typeof data === "undefined") return defaultValue;
+        return data;
+    },
+    Has(key) {
+        return this.Cache.has(key);
+    },
+    Remove(key) {
+        this.Cache.delete(key);
+        return this;
+    },
+    Clear() {
+        this.Cache.clear();
+        return this;
+    }
+};
+
+const CreateCache = () => ({
+    ...CCache,
+    Cache: new Map()
+})
